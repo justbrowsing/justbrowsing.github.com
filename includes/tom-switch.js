4a558a283645ba9266913a0be48c50594b80dd80
justbrowsing <developer4linux@gmail.com>
2013-07-02 06:12:50 -0700

    jquery start guide

---
+++ includes/tom-switch.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+
+  $('a.choice').click(function(e){
+    /*e.preventDefault();*/
+
+    $(this).siblings('.choice.current').removeClass('current');
+    $(this).addClass('current');
+
+    START = $(this).attr('href').substr(1);
+
+    if($(this).siblings('div.START.current').size())
+    {
+      switchFrom($(this).siblings('div.START.current'), START);
+    }
+    else
+    {
+      switchTo($('#START' + START));
+    }
+
+    history.pushState(null, null, $(this).attr('href'));
+  });
+
+  function switchFrom(START, toSTART)
+  {
+    $(START).slideUp(600,function(){
+      $(this).removeClass('current');
+      $(this).children('a.choice').removeClass('current');
+      $(this).children('div.START').each(function(){
+        $(this).hide();
+      });
+
+      switchTo($('#START' + toSTART));
+    });
+  }
+
+  function switchTo(START)
+  {
+    $(START).addClass('current');
+    $(START).slideDown(600);
+  }
+
+  $('.step2').hide();
+
+  /* autoselect */
+  $('#chooseCD').addClass('current');
+  switchTo($('#STARTcd'));
+  var detectOS = navigator.platform;
+  if (detectOS == "Win32") {
+    $('#chooseCD_WIN6').addClass('current');
+    switchTo($('#STARTcd_win6'));
+  }
+  else if (detectOS == "Win64") {
+    $('#chooseCD_WIN7').addClass('current');
+    switchTo($('#STARTcd_win7'));
+  }
+  else if (detectOS == "Linux i686" || detectOS == "Linux x86_64") {
+    $('#chooseCD_LINUXG').addClass('current');
+    switchTo($('#STARTcd_linuxg'));
+  }
+  else if (detectOS == "MacIntel") {
+    $('#chooseCD_OSX').addClass('current');
+    switchTo($('#STARTcd_osx'));
+  }
+  else {
+    $('#chooseCD_LINUXK').addClass('current');
+    switchTo($('#STARTcd_linuxk'));
+  }
+});
+
